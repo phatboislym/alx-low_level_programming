da8ea5de166fce1637a96171aa6452328d33b2c7
phatboislym <phatboislym@protonmail.com>
2022-06-02 14:45:21 -0700

    add:3-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
