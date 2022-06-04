a36858930bd7c38e26b377d95cf8698aaf782bf4
phatboislym <phatboislym@protonmail.com>
2022-06-04 09:51:44 -0700

    readd: 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,10 +15,10 @@ int main(void)
 {
 int i;
 char a;
-for (i = '0', i <= '9', ++i)
-putchar(i);
+for (i = '0'; i <= '9'; ++i)
+putchar((i) + '0');
 
-for (a = 'a', a <= 'f', ++a)
+for (a = 'a'; a <= 'f'; ++a)
 putchar(a);
 
 putchar('\n');
