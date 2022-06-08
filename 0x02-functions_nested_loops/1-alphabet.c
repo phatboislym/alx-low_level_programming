2fe97bef71fc114cd2b4badab66398cdd77d68ed
phatboislym <phatboislym@protonmail.com>
2022-06-08 16:01:27 -0700

    add: edited 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * program prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
