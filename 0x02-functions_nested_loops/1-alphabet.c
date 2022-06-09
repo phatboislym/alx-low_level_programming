0004553b58c91f3e3764a68157581892bc912811
phatboislym <phatboislym@protonmail.com>
2022-06-09 02:57:26 -0700

    add: 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  *
- * Return: void
  */
 
 void print_alphabet(void)
