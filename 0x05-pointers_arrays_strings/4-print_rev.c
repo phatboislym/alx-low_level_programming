c7e8a27588347249b4c2a688efbffd73c3c50537
phatboislym <phatboislym@protonmail.com>
2022-06-14 05:29:02 -0700

    add: 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line 
+ * @s: input string parameter
+ *
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (l >= 0)
+	{
+		if (s[l] == '\0')
+		{
+			break;
+		}
+		l++;
+	}
+	for (l--; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}
