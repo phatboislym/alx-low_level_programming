6781f89686571c6cc67062f111fc87615d66f64d
phatboislym <phatboislym@protonmail.com>
2022-06-14 07:44:31 -0700

    add: 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,22 +3,17 @@
 /**
  * puts2 - prints every other character of a string
  * starting with the first character, followed by a new line
- * @str: input string parameter
-*/
+ * Return: every other character from the string (1, 3, 5, 7 etc)
+ */
 
 void puts2(char *str)
 {
-int i, j = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
