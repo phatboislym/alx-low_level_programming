7b3a3a2e378b0861b54ad37bffeeb9ddf1091b0a
phatboislym <phatboislym@protonmail.com>
2022-06-14 07:46:22 -0700

    readd: 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,7 @@
 /**
  * puts2 - prints every other character of a string
  * starting with the first character, followed by a new line
+ * @str: string parameter
  * Return: every other character from the string (1, 3, 5, 7 etc)
  */
 
