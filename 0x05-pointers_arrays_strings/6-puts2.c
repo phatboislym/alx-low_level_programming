5a7db1e0940df44b0461d989efb77d9de0e9f8c4
phatboislym <phatboislym@protonmail.com>
2022-06-14 06:06:49 -0700

    add: 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: input string parameter
+*/
+
+void puts2(char *str)
+{
+int i, j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
+	}
+	_putchar('\n');
+}
