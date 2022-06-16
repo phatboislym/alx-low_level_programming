acaab13278d1e5b7f791d2aed4ac32801089d864
phatboislym <phatboislym@protonmail.com>
2022-06-16 12:41:44 -0700

    readd: 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcmp - compares two strings
- * @*s1 - string input 1
- * @*s2 - string input 2
+ * @s1: - string input 1
+ * @s2: - string input 2
  * Return: the difference between s1 and s2
  * negative int if s1 < s2, 0 if s1 == s2, positive int if s1 > s2
  */
