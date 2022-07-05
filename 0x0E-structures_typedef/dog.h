f0b4d31ac6eee89bf4db943a198c8e48a093f23f
phatboislym <phatboilsym@protonmail.com>
2022-07-05 01:18:36 +0100

    add: files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type describing a dog
+ * @name: char pointer, name of the dog
+ * @age: float, age of the dog
+ * @owner: char pointer, owner of the dog
+ * desc: struct for new dog, three members
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog type
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ */
