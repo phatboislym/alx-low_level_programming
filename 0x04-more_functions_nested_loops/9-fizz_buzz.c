1fdb49e88262bded247dc5710b3f868a64ad50e9
phatboislym <phatboilsym@protonmail.com>
2022-07-28 14:44:55 +0100

    add: 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,25 @@
 int main(void)
 {
 int i = 1;
-for (int i = 1; i < 101; i++)
+for (i = 1; i < 101; i++)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
-printf("FizzBuzz\n ");
-else if (i % 3 == 0)
-printf("Buzz\n ");
-else if (i % 5 == 0)
-printf("Fizz\n ");
-else
-printf("%d\n ", i);
+	if ((i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+		printf("%i ", i);
+	}
 }
-
+printf("\n");
 return (0);
 }
