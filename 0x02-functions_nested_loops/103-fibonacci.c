3bf0cad76768fb2b520e0dee92e61b7f6c65a8ef
phatboislym <phatboilsym@protonmail.com>
2022-07-29 01:57:21 +0100

    add: 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,7 +2,8 @@
 
 /**
  * main -  finds and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line
+ * starting with 1 and 2, followed by a new line. finds and
+ * prints the sum of the even-valued terms, followed by a new line
  *
  * Return: 0
  */
@@ -14,13 +15,19 @@ unsigned long x = 0;
 unsigned long y = 1;
 unsigned long sum = 0;
 unsigned long runningSum = 0;
+unsigned long evenSum = 0;
+
 for (i = 0; i < 32; i++)
 {
-sum = (x + y);
+	sum = (x + y);
+if (sum % 2 == 0)
+{
+	evenSum = evenSum + sum;
+}
 x = y;
 y = sum;
 runningSum += sum;
 }
-printf("%lu\n", runningSum);
+printf("%lu\n", evenSum);
 return (0);
 }
